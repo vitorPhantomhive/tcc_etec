5924bd32663a4a5065fb793c6477d385d9be77f2
vitor_phantomhive <vitorkhaled47@gmail.com>
2023-12-30 00:34:26 -0300

    iniciando front end e guardando ideias para o back

---
+++ front-end/Study_Home/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import Formulario from './components/Formulario'
+
+function App() {
+  return (
+    <div>
+      <Formulario></Formulario>
+    </div>
+  )
+}
+
+export default App
---
+++ front-end/Study_Home/src/components/Formulario/index.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Formulario(){
+    return(
+        <div>
+            <label htmlFor="">Testando componentes</label>
+            {/* <input type="text"></input> */}
+        </div>
+    )
+}
